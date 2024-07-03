6e9b4a8713e82c832d648fca12722c355aaf93af
Raifatul Jannat Sheripta <132344220+Sheripta@users.noreply.github.com>
2024-07-03 20:59:57 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,147 @@
+// console.log("Hello,World")
+
+// alert("Javacscript is connected")
+
+//   document.write("Welcome to Javacscript")
+
+//  console.log("Welcome to Javacscript")
+
+//  alert("Welcome to Javacscript")
+ 
+// JS syntax
+
+//  let x = 10;
+//  if (x>5) {
+//     console.log("x is geater than 5");
+//  }
+// else {
+//    console.log("x is not geater than 5");
+// }
+
+// JS Variables
+// let name="John";
+// let age=25;
+// console.log('name');
+// console.log('age');
+
+
+//  JS let
+
+//  let x = 7;
+//  {
+//     let x=6;
+//     console.log(x);
+//  }
+//  console.log(x);
+
+//  JS const
+
+
+// JS operators
+
+// let y=8;
+// let z=5;
+// let sum=y+z;
+// console.log(sum);
+// let subtraction=y-z;
+// console.log(subtraction);
+// let multiplication=y*z;
+// console.log(multiplication);
+// let division=y/z;
+// console.log(division);
+// let modulus=y%z;
+// console.log(modulus);
+
+
+// JS Arithmetic
+// var a=Number(prompt("enter first number"));
+
+// JS Assignment
+// let a=10;
+// a+=5;
+// console.log('value of a is:'+a);
+
+// let a=10;
+// a-=5;
+// console.log('value of a is:'-a);
+
+
+// let a=10;
+// a*=5;
+// console.log('value of a is:'*a);
+
+
+// let a=10;
+// a%=5;
+// console.log('value of a is:'%a);
+
+// JS Data Types
+
+// let a="John"
+// console.log(typeof(a));
+// let b=3;
+// console.log(typeof(b));
+// let c=true;
+// console.log(typeof(c));
+// let person={
+//    firstname:"John",
+//    lastname:"Doe",
+// }
+// console.log(typeof(person));
+
+// let d =["3"]
+// console.log(typeof(d));
+
+// // JS Functions
+// function sum(a,b){
+//    return a+b;
+// }
+// const value =sum(1,4);
+// console.log(value);
+
+// JS Objects
+
+// const studentInfo={
+//    firstname:"Jhon",
+//     lastname:"Doe",
+//     age:25,
+//     grade:"5.00",
+//    }
+// console.log(studentInfo);
+
+
+// JS Object Properties
+
+// Adding properties using dot notation
+// studentInfo.country = 'BD';
+// studentInfo.email = 'jhon@example.com';
+ 
+// // Adding properties using bracket notation
+// studentInfo['hobby'] = 'Gaming';
+// studentInfo['gender'] = 'Male';
+ 
+// console.log('After adding properties:');
+// console.log(studentInfo);
+ 
+
+// // Removing properties using dot notation
+// delete studentInfo.age;
+// delete studentInfo.grade;
+ 
+// // Removing properties using bracket notation
+// delete studentInfo['hobby'];
+// delete studentInfo['gender'];
+ 
+// console.log('After removing properties:');
+// console.log(studentInfo);
+
+
+// Exercise 2
+// JS object properties
+const bookInfo = {};
+bookInfo["Title"] = "The Book";
+bookInfo["Author"] = "Famous Writer";
+bookInfo["Year"] = "1984";
+
+console.log(bookInfo);
+
